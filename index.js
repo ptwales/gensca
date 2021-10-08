fa139ded52f323e9c556bed6bf17e40bb216e680
Philip Wales <me@ptwales.net>
2021-10-08 15:05:14 -0500

    Update to use transform-json-types

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 #!/usr/bin/env node
 
 const fs = require("fs");
-const GenerateSchema = require("generate-schema");
-const SchemaConverter = require("json-schema-to-case-class");
 const cli = require("cli");
 const util = require("util");
+const transform = require("transform-json-types");
 
-const scalaModelsFromJsonData = (name, config, json) =>
-  new Promise((resolve) => resolve(GenerateSchema.json(name, json)))
-    .then((schema) => (schema.type === "array" ? schema.items : schema))
-    .then((schema) => SchemaConverter.convert(schema, config));
+const main = () => {
+  const options = cli.parse({
+    file: ["f", "A json file to read", "file", false],
+    name: ["n", "Root name of the class", "name", "Response"],
+  });
+  const transformConfig = { lang: "scala", objectName: options.name };
+  let json = options.file ? jsonFromFile(options.file) : jsonFromStdin();
+  json
+    .then((json) => scalaModelsFromJsonData(transformConfig, json))
+    .then((scala) => cleanUpCode(scala))
+    .then((scala) => addCodecs(scala))
+    .then((scala) => console.log(scala))
+    .catch((err) => console.error(err));
+};
+
+const scalaModelsFromJsonData = (config, json) =>
+  new Promise((resolve) => resolve(transform(json, config)));
 
 const readStdin = () =>
   new Promise((resolve) => cli.withStdin((text) => resolve(text)));
@@ -21,16 +33,21 @@ const jsonFromFile = (path) =>
     .promisify(fs.readFile)(path, "utf-8")
     .then((data) => JSON.parse(data));
 
-const main = () => {
-  const options = cli.parse({
-    file: ["f", "A json file to read", "file", false],
-  });
-  const scalaConfig = { classParamsTextCase: "ignoreCase" };
-  let json = options.file ? jsonFromFile(options.file) : jsonFromStdin();
-  json
-    .then((json) => scalaModelsFromJsonData("name", scalaConfig, json))
-    .then((scala) => console.log(scala))
-    .catch((err) => console.error(err));
+const cleanUpCode = (scalaCode) => scalaCode.replace(/\s\(/g, "(");
+
+const addCodecs = (scalaCode) => {
+  const caseClasses = listCaseClasses(scalaCode);
+  const codecs = caseClasses.map(formatCodec);
+  return [scalaCode, ...codecs].join("\n");
 };
 
+const listCaseClasses = (scalaCode) =>
+  [...scalaCode.matchAll(/case class (\w+)\(/g)].map((m) => m[1]);
+
+const formatCodec = (className) => {
+  const camelCase = className[0].toLowerCase() + className.slice(1);
+  return `implicit val ${camelCase}Decoder: Decoder[${className}] = deriveDecoder[${className}]`;
+}
+
+// Execute
 main();
